812cf31cccee85949b19f9cc79ad2e98d73eb105
sattarkarashi <satokarashi@gmail.com>
2025-01-16 20:19:45 +0100

    Added chroot to the project to isolate the root from the main os

--- isolated_process.py
+++ isolated_process.py
@@ -2,15 +2,23 @@ import os
 
 def run():
     pid = os.fork()
+    print("Starting process")
+    print(pid)
 
     if pid == 0:
         print("Child process")
-        os.execvp(file="/bin/bash", args=["bash"])
+        # Change root directory
+        print(os.getcwd())
+        os.chroot("/home/sato/what_I_learned_today/docker/docker_course/myroot")
+        os.chdir("/")
+        os.execvp(file="/bin/bash", args=["/bin/bash"])
 
     else:
         rid, status = os.waitpid(pid, 0)
         print(rid, status)
         print("Parent process")
+    
+    print("Finishing process")
 
 
 if __name__ == "__main__":
